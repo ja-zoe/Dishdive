fd6697e8462e538ece07eac350149a645260db60
Julian Vilfort <j.vilfort01@gmail.com>
2024-04-17 20:18:03 -0400

    created client and server directories

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,9 +1,13 @@
-import React from "react";
+import { React, useState } from "react";
 import styles from '../styles/SignUpForm.module.css'
 import classNames from "classnames";
 import { Link } from "react-router-dom";
 
 const SignUpForm = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = ''
+    const [password, setPassword] = useState('')
+
     return (
         <div className={styles.container}>
             <div className={styles.signUp}>
@@ -11,15 +15,15 @@ const SignUpForm = () => {
                 <form className={styles.form}>
                     <label className={styles.label}>
                         <span className={styles.span}> Username </span>
-                        <input type='text' name='username' className={styles.input}></input>
+                        <input type='text' name='username' className={styles.input} onChange={(e) => {setUsername(e.target.value)}}></input>
                     </label>
                     <label className={styles.label}>
                         <span className={styles.span}> Email </span>
-                        <input type='email' name='email' className={styles.input}></input>
+                        <input type='email' name='email' className={styles.input} onChange={(e) => {setEmail(e.target.value)}}></input>
                     </label>
                     <label className={classNames(styles.label, styles.passwordLabel)}>
                         <span className={styles.span}> Password </span>
-                        <input type='password' name='password' className={styles.input}></input>
+                        <input type='password' name='password' className={styles.input} onChange={(e) => {setPassword(e.target.value)}}></input>
                     </label>
                     <Link to='/login' className={styles.accountAlready}> Already Have an Account? </Link>
                     <button className={styles.signUpButton}> Sign Up </button>
