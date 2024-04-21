3a70b00086b2499193c26d51642083627a13e61d
Julian Vilfort <j.vilfort01@gmail.com>
2024-04-20 22:15:09 -0400

    register function fully made and error handling added

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -8,6 +8,28 @@ const SignUpForm = () => {
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [registrationStatus, setRegistrationStatus] = useState('')
+
+    const register = (event) => {
+        event.preventDefault()
+        if(email.length>0&&password.length>0&&username.length>0){
+            axios.post('http://localhost:3310/register', {
+                username: username,
+                email: email,
+                password: password 
+            }).then((response)=>{
+                    setRegistrationStatus(response.data.message)
+                })
+        }else{
+            if(username.length==0){
+                setRegistrationStatus('Please enter a username!')
+            }else if(email.length==0){
+                setRegistrationStatus('Please enter an email!')
+            }else{
+                setRegistrationStatus('Please enter a password!')
+            }
+        }
+    }
 
     return (
         <div className={styles.container}>
@@ -16,18 +38,19 @@ const SignUpForm = () => {
                 <form className={styles.form}>
                     <label className={styles.label}>
                         <span className={styles.span}> Username </span>
-                        <input type='text' name='username' className={styles.input}></input>
+                        <input type='text' name='username' className={styles.input} onChange={(e)=>{setUsername(e.target.value)}}></input>
                     </label>
                     <label className={styles.label}>
                         <span className={styles.span}> Email </span>
-                        <input type='email' name='email' className={styles.input}></input>
+                        <input type='email' name='email' className={styles.input} onChange={(e)=>{setEmail(e.target.value)}}></input>
                     </label>
                     <label className={classNames(styles.label, styles.passwordLabel)}>
                         <span className={styles.span}> Password </span>
-                        <input type='password' name='password' className={styles.input}></input>
+                        <input type='password' name='password' className={styles.input} onChange={(e)=>{setPassword(e.target.value)}}></input>
                     </label>
+                    <p className={styles.registrationStatus}>{registrationStatus}</p>
                     <Link to='/login' className={styles.accountAlready}> Already Have an Account? </Link>
-                    <button className={styles.signUpButton}> Sign Up </button>
+                    <button className={styles.signUpButton} onClick={register}> Sign Up </button>
                 </form>
             </div>
         </div>
