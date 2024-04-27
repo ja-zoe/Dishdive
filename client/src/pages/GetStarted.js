fc3a645457d4559f955276006f8efc8ce9f68149
Julian Vilfort <j.vilfort01@gmail.com>
2024-04-26 20:55:37 -0400

    set up get started page for ingredient addition

--- client/src/pages/GetStarted.js
+++ client/src/pages/GetStarted.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import { React, useState } from 'react';
 import styles from '../styles/GetStarted.module.css'
 
 function GetStarted() {
+    const [ingredient, setIngredient] = useState('')
+    const [ingredientsList, setIngredientsList] = useState([])
+
+    const addIngredient = (e) => {
+        e.preventDefault()
+        const newList = [...ingredientsList, ingredient]
+        setIngredientsList(newList)
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.shareIngredients}>
                 <h1 className={styles.pleaseShare}> Please Share the Ingredients You'd Like to Use</h1>
                 <p className={styles.typeIngredients}>Type each ingredient seperately that you want to want to use in your dish. <span className={styles.minimum}> Minimum of 3 ingredients required. </span></p>
-                <form>
-                    <input placeholder='Enter Your Ingredients'></input>
+                <form onSubmit={addIngredient}>
+                    <input placeholder='Enter Your Ingredients' onChange={(e)=>setIngredient(e.target.value)}></input>
                 </form>
             </div>
             <div className={styles.yourIngredients}>
                 <h1 className={styles.pleaseShare}> Your Ingredients</h1>
+                <div className={styles.ingredientsList}>
+                    <p> 
+                    
+                    </p>
+                </div>
             </div>
         </div>
     )
