7e326c2207e0ccd3577860d673d0b7a59d6e0e6c
Julian Vilfort <j.vilfort01@gmail.com>
2024-05-02 00:19:16 -0400

    added styles to input element in share ingredients

--- client/src/pages/GetStarted.js
+++ client/src/pages/GetStarted.js
@@ -23,14 +23,14 @@ function GetStarted() {
          setIngredientsList(newList)
     }
 
-
+    console.log(ingredientsList)
     return (
         <div className={styles.container}>
             <div className={styles.shareIngredients}>
                 <h1 className={styles.pleaseShare}> Please Share the Ingredients You'd Like to Use</h1>
                 <p className={styles.typeIngredients}>Type each ingredient seperately that you want to want to use in your dish. <span className={styles.minimum}> Minimum of 3 ingredients required. </span></p>
-                <form onSubmit={addIngredient}>
-                    <input placeholder='Enter Your Ingredients' onChange={(e)=>setIngredient(e.target.value)}></input>
+                <form onSubmit={addIngredient} className={styles.form}>
+                    <input placeholder='Enter Your Ingredients' onChange={(e)=>setIngredient(e.target.value)} className={styles.enterIngredients}></input>
                 </form>
             </div>
             <div className={styles.yourIngredients}>
